e535129f6fa1ede38aa4ae4d22cb109bf9da3e30
Roma Zubchik <Bumblebeepro17@gmail.com>
2023-05-16 18:35:04 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "db4free.net",
+      user: "romazubchik",
+      password: "12345678",
+      database: "myproject325",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
